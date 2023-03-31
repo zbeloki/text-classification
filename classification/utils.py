efcfa01f137e06d3a08de403f242dedba3d9214b
Zuhaitz Beloki <z.beloki@elhuyar.eus>
2023-03-31 10:58:41 +0200

    DatasetSplit fully tested
    Former-commit-id: fcc3080a63c7d06392c20b7ac68abf0b13cd5579

--- classification/utils.py
+++ classification/utils.py
@@ -42,24 +42,23 @@ def lemmatize(texts, dic_path):
 
   return stem_col
 
-def oversample(X, y, ids=None, target_f=np.average):
+def oversample(X, y, target_f=np.average):
 
-    if ids is None:
-        ids = list(range(len(y)))
+  ids = list(range(len(y)))
         
-    class_counts = np.sum(y, axis=0)
-    class_avg = target_f(class_counts)
+  class_counts = np.sum(y, axis=0)
+  class_avg = target_f(class_counts)
 
-    rates_per_class = np.copy(class_counts).astype(float)
-    rates_per_class[rates_per_class > 0] = class_avg / class_counts[class_counts > 0]
-    rates_per_class[(rates_per_class > 0) & (rates_per_class < 1.0)] = 1.0
-    rates_per_class = rates_per_class.round().astype(int)
+  rates_per_class = np.copy(class_counts).astype(float)
+  rates_per_class[rates_per_class > 0] = class_avg / class_counts[class_counts > 0]
+  rates_per_class[(rates_per_class > 0) & (rates_per_class < 1.0)] = 1.0
+  rates_per_class = rates_per_class.round().astype(int)
 
-    rates_per_example = np.max(np.where(y == 1, rates_per_class, 0), axis=1)
-    y_oversampled = np.repeat(y, rates_per_example, axis=0)
-    X_oversampled = np.repeat(X, rates_per_example, axis=0)
-    ids_oversampled = np.repeat(ids, rates_per_example, axis=0)
+  rates_per_example = np.max(np.where(y == 1, rates_per_class, 0), axis=1)
+  y_oversampled = np.repeat(y, rates_per_example, axis=0)
+  X_oversampled = np.repeat(X, rates_per_example, axis=0)
+  ids_oversampled = np.repeat(ids, rates_per_example, axis=0)
+  
+  shuf = np.random.permutation(len(ids_oversampled))
 
-    shuf = np.random.permutation(len(ids_oversampled))
-
-    return X_oversampled[shuf], y_oversampled[shuf], ids_oversampled[shuf]
+  return X_oversampled[shuf], y_oversampled[shuf], ids_oversampled[shuf]
